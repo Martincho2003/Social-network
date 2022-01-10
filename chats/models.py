6d255e7f57c6428f0faaa94feb42389586577dfe
Martincho2003 <martindinev2003@gmail.com>
2022-01-10 19:30:05 +0200

    Add and fix sqllite tables

--- chats/models.py
+++ chats/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 class Chat(models.Model):
     chat_name = models.CharField(max_length=100)
+    chat_memebers_count = models.IntegerField(default=2)
+    chat_owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+class ChatAdmin(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    admin_chat = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
 class Message(models.Model):
-    message_text = models.CharField()
+    message_text = models.CharField(max_length=280)
     message_time = models.DateTimeField()
-    sender_id = models.ForeignKey(auth_user)
-    chat_id = models.ForeignKey(Chat)
+    sender = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
