068f3b3ce1231ae7291fa3e4e2787905a57d2a78
Martincho2003 <48617764+Martincho2003@users.noreply.github.com>
2022-04-10 15:47:41 +0300

    Adding working test for register and login

--- chats/test_login.py
+++ chats/test_login.py
@@ -6,7 +6,14 @@ class LoginTest(TestCase):
     def setup(self):
         self.client = Client()
 
-    # def test_valid_login(self):
-    #     data = json.dumps({"username": "marti", "password": "Aa123456"})
-    #     response = self.client.post('/chats/login', data, content_type='application/json')
-    #     self.assertEqual(json.loads(response.content)["status"], "successful")  
+    def test_valid_login(self):
+        register_data = json.dumps({"username": "marti",
+                            "first_name": "Martin",
+                            "last_name": "Dinev",
+                            "email": "mars@gmail.com",
+                            "password1": "Aa123456",
+                            "password2": "Aa123456"})
+        self.client.post('/chats/register', register_data, content_type='application/json')
+        login_data = json.dumps({"username": "marti", "password": "Aa123456"})
+        response = self.client.post('/chats/login', login_data, content_type='application/json')
+        self.assertEqual(json.loads(response.content)["status"], "successful")  
--- chats/test_register.py
+++ chats/test_register.py
@@ -15,13 +15,20 @@ class RegisterTest(TestCase):
         response = self.client.post('/chats/register', data, content_type='application/json')
         self.assertEqual(json.loads(response.content)["status"], "successful")
 
-    # def test_repeated_username(self):
-    #     data = json.dumps({"username": "marti",
-    #                         "first_name": "Martin",
-    #                         "last_name": "Dinev",
-    #                         "email": "mars@gmail.com",
-    #                         "password1": "Aa123456",
-    #                         "password2": "Aa123456"})
-    #     response = self.client.post('/chats/register', data, content_type='application/json')
-    #     self.assertEqual(json.loads(response.content)["status"], "unsuccessful")
-    #     self.assertEqual(json.loads(response.content)["error"], "Username already exist! Please try other username.")
+    def test_repeated_username(self):
+        data = json.dumps({"username": "marti",
+                            "first_name": "Martin",
+                            "last_name": "Dinev",
+                            "email": "mars@gmail.com",
+                            "password1": "Aa123456",
+                            "password2": "Aa123456"})
+        self.client.post('/chats/register', data, content_type='application/json')
+        data2 = json.dumps({"username": "marti",
+                            "first_name": "Martin",
+                            "last_name": "Dinev",
+                            "email": "mars@gmail.com",
+                            "password1": "Aa123456",
+                            "password2": "Aa123456"})
+        response = self.client.post('/chats/register', data2, content_type='application/json')
+        self.assertEqual(json.loads(response.content)["status"], "unsuccessful")
+        self.assertEqual(json.loads(response.content)["error"], "Username already exist! Please try other username.")
